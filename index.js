b525de2828ffff2f22cbd8ec59c26e21fae220ba
Mesut KILINCASLAN <kilincaslanmesut@gmail.com>
2023-02-16 15:41:37 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.get('/api/nylas-webhooks', (req, res) => {
+    console.log('query >>>>>>', req.query);
+
+    return res.send(req.query.challenge);
+});
+
+app.post('/api/nylas-webhooks', (req, res) => {
+    console.log('body >>>>>>>', req.body);
+
+    return res.status(200).end();
+});
+
+app.listen(3001, () => {});
